ca2edeae63dfd2594c19777f77db1f79de5d59b3
Will <wtdavis@wesleyan.edu>
2024-09-07 23:06:13 -0400

    leet bb

--- palindrome_number.js
+++ palindrome_number.js
@@ -4,34 +4,33 @@
  */
 var isPalindrome = function(x) {
 
-    //recursion: take x%10 to get 1's place digit. divide by 10 to get everything but 1's, recur
+    let res = null
 
-    let palRecursion = function(num, arr) {
+    let recursionReturnValue = palRecur(x)
 
-        if (num === 0) {
-            return arr
-        }
+    if (recursionReturnValue === x) {
 
-        if (num) {
-        let tmp0 = num % 10
-        let tmp1 = Math.floor(num/10)
-        }
-
-        
-
-        if (tmp0 ) {
-            arr = arr.concat(tmp0)
-            palRecursion(tmp1, arr)
-        } 
-    
     }
 
-    //final return: true/false based on equivalency between recursive result and input integer 'x'
-    let res = []
 
-    if (palRecursion(x, res) === x) {
-        return true
-    } else {
-        return false
-    }
+
+    return res
 };
+
+const palRecur = function (num, arr) {
+    let res = arr | [];
+
+    let digit = num % 10;
+    let next = Math.floor(num/10)
+    if (next === 0) {
+        
+    }
+
+    res.push(digit)
+    palRecur(next, res)
+}
+
+
+isPalindrome(202)
+isPalindrome(20)
+isPalindrome(1010)
