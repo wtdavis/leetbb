97e33c1c39b3939eeb232915ad9617e86df8149d
Will <wtdavis@wesleyan.edu>
2024-09-28 22:18:35 -0400

    killin it

--- merge_sorted_arr.js
+++ merge_sorted_arr.js
@@ -1,3 +1,11 @@
 const mergeSortedArr = function (num1, num2, m, n) {
-    
+    let arr = [];
+
+    for (let i=0;i<(m+n);i++){
+        arr.push(num1.pop())
+    }
+    console.log(arr)
 }
+
+mergeSortedArr([1,2,3,4,0,0,0], [2,4,6], 4, 3)
+1
