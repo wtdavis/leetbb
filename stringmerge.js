d4a990bce046d503947085e9ef2a54e2713d113b
Will <wtdavis@wesleyan.edu>
2025-03-08 23:10:18 -0500

    test

--- stringmerge.js
+++ stringmerge.js
@@ -35,30 +35,8 @@ const mergeAlternively = (word0, word1) => {
         console.log(resArr)
     }
     1
-    2
-    3
-    4
-    5
-    6
-    7
-    8
-    9
+    
 
-    1
-    2
-    3
-    4
-    5
-    6
-    7
-    8
-    1
-    2
-    3
-    4
-    5
-    6
-    7
 
 
 derp
